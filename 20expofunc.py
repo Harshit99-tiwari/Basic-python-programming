9b9eb4c2dbfbda1fd41e77f78b35f3a95d3dbfa1
Harshit99-tiwari <tiwariankush656@gmail.com>
2026-01-11 00:25:09 +0530

    Update 20expofunc.py

--- 20expofunc.py
+++ 20expofunc.py
@@ -1,9 +1,12 @@
-mates=["adam" , "john" , "rossy" , "harsh"]
-for mate in mates:
-    print(mate)
+print(3**4)
+print(2**2)
+print(5**2)
 
-for words in "Light", "house":
-    print(words)
+def power_by(base_number , powered_number):
+     result=1
+     for index in range(powered_number):
+          result = result * base_number
+     return result
+     
 
-for words in "Light house":
-    print(words)
+print(power_by(5,2))
