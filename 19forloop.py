c7665546d3db2889edea3a9da03f81577dfd0b98
Harshit99-tiwari <tiwariankush656@gmail.com>
2026-01-11 00:24:13 +0530

    Update 19forloop.py

---
+++ 19forloop.py
@@ -0,0 +1,29 @@
+mates=["adam" , "john" , "rossy" , "harsh"]
+for mate in mates:
+    print(mate)
+
+for words in "Light", "house":
+    print(words)
+
+for words in "Light house":
+    print(words)
+
+for number in range(12):
+    print(number)
+
+for number in range(5, 12):
+    print(number)
+
+student=["harsh" , "harshit" , "kansh"]
+for index in range(len(student)):
+    print(student[index])
+
+students=["harsh" , "harshit" , "kansh"]
+print(len(students))
+
+subject = ["math" , "physics" , "chemistry" , "biology"]
+for index in range(4):
+    if index==0:
+     print("First number")
+    else:
+        print("second number")
